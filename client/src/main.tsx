f8a79d03c7df8522c3e42cb8b5cb8ed70f57f62b
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-08-12 22:30:10 +0800

    add clerk for auth, tanstack router for route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { routeTree } from "./routeTree.gen.ts";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const queryClient = new QueryClient();
+
+// Create a new component to wrap everything
+const Root = () => (
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <App />
+      </QueryClientProvider>
+    </ClerkProvider>
+  </React.StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<Root />);
