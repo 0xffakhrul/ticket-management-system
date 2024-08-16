24d8f6c73150671b3ea853170d575fb7dc5e402b
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-08-16 22:02:46 +0800

    add landing page

---
+++ client/src/pages/landing/index.tsx
@@ -0,0 +1,17 @@
+export const Landing = () => {
+  return (
+    <div className="w-11/12 max-w-6xl mx-auto py-20 flex flex-col gap-8">
+      <h1 className="text-center font-bold text-6xl max-w-2xl mx-auto tracking-normal">
+        Streamline Your Workflow with Ease
+      </h1>
+      <p className="max-w-3xl mx-auto text-center text-gray-600">
+        Empower your team with real-time updates, customizable priorities, and
+        seamless communication, all in one place. Say goodbye to chaos and hello
+        to organized productivity.
+      </p>
+      <button className="rounded-full bg-black text-white px-4 py-2 max-w-36 mx-auto">
+        Start Ticketing
+      </button>
+    </div>
+  ); 
+};
