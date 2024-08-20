57d05cd006ba4a7949726988ec82a7c055fd1d2e
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-08-20 10:20:15 +0800

    ui fix

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -2,8 +2,8 @@ import { TicketCards } from "./TicketCards";
 
 export const Dashboard = () => {
   return (
-    <div className="bg-black py-4 px-8">
-      <div className="max-w-7xl mx-auto">
+    <div className="bg-black py-4 px-8 ">
+      <div className="w-11/12 max-w-6xl mx-auto">
         <h1 className="text-white font-bold text-2xl pb-4">Dashboard</h1>
         <TicketCards />
       </div>
